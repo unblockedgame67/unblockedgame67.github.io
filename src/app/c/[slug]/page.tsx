7a1de95ecd764680de2235d8231c5f4243f1f73e
yarops <ed.creater@gmail.com>
2024-09-17 13:36:56 +0300

    Initial commit

---
+++ src/app/c/[slug]/page.tsx
@@ -0,0 +1,45 @@
+import CategoryLayout from "@/app/c/[slug]/layout";
+import {getAllCategories, getCategoryBySlug} from '@/utils/lib/categories';
+import {getPaginatedPostsByCategoryId} from '@/utils/lib/posts';
+
+import TemplateArchive from '@/templates/TemplateArchive';
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+  title: "",
+};
+
+export default async function Category({ params }: { params: { slug: string } }) {
+  const {category} = await getCategoryBySlug(params.slug);
+
+  if (!category) {
+    return {};
+  }
+
+  if (category.seo) {
+    metadata.title = category.seo.title;
+    metadata.description = category.seo.description;
+  }
+
+  const {id, title, content, slug} = category;
+
+  if (!category) {
+    return {
+      props: {},
+      notFound: true,
+    };
+  }
+
+  const {posts, pagination} = await getPaginatedPostsByCategoryId(id);
+  pagination.basePath = '/c/' + slug;
+
+  return <CategoryLayout>
+  <TemplateArchive
+    title={title}
+    posts={posts}
+    pagination={pagination}
+    slug={slug}
+    content={content}
+  />
+  </CategoryLayout>;
+}
